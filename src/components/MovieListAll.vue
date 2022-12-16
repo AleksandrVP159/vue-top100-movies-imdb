fc99ee3f128b85c50ef3375928991682c289c70f
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-16 20:47:56 +0500

    add movielistall

---
+++ src/components/MovieListAll.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="movie-list" class="container w-full px-4 mx-auto">
+        <div>
+            <h3 class="text-4xl mb-6 mt-4 text-white">
+                IMDB All Films
+            </h3>
+            <!--   <Search /> -->
+            <h3 class="text-4xl mb-6 mt-4 text-white">
+                Films with the title: {{ moviesStore.searchValue }}
+            </h3>
+        </div>
+        <div class="grid gap-4 grid-cols-5">
+            <MovieItem v-for="(movie, key) in moviesStore.moviesAll" :key="key" :movie="movie"
+                @mouseover.native="onMouseOver(movie.Poster)" @mouseout.native="onMouseOut('')" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { useMovieStore } from '../stores/movies.js'
+import { computed } from 'vue';
+import MovieItem from './MovieItem.vue';
+/* import Search from './Search.vue'; */
+
+const moviesStore = useMovieStore()
+
+const emits = defineEmits(['changePoster', 'changeOutPoster'])
+
+function onMouseOver(poster) {
+    emits('changePoster', poster)
+}
+
+function onMouseOut(poster) {
+    emits('changeOutPoster', poster)
+}
+
+</script>
