6886f999eba2e82626dddd3cfb2a6c2f3983e99b
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-16 20:49:40 +0500

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,28 @@
 <template>
     <PosterBg :poster="posterBg" />
-    <MoviesList @change-poster="onChangePoster" @change-out-poster="onChangeOutPoster" />
+    <MoviesList v-if="!isExistAll" @change-poster="onChangePoster" @change-out-poster="onChangeOutPoster" />
+    <MovieListAll v-else @change-poster="onChangePoster" @change-out-poster="onChangeOutPoster" />
     <Auth v-if="modalStore.isOpen === true" />
+    <Search />
 </template>
 
 <script setup>
 import { useModalStore } from '../stores/modal';
+import { useMovieStore } from '../stores/movies';
 import MoviesList from '../components/MoviesList.vue';
+import MovieListAll from '../components/MovieListAll.vue'
+import Search from '../components/Search.vue';
 import PosterBg from '../components/PosterBg.vue';
 import Auth from '../components/Auth.vue';
 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const posterBg = ref('')
 
 const modalStore = useModalStore()
+const movieStore = useMovieStore()
+
+const isExistAll = computed(() => Boolean(Object.keys(movieStore.moviesAll).length))
 
 function onChangePoster(poster) {
     posterBg.value = poster
