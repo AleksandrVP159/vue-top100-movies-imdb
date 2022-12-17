dbf6e55b02cd434a182ea56dae61cdc64e81cf24
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-17 14:15:29 +0500

    add movieDetail

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const Home = () => import("../pages/Home.vue");
 const About = () => import("../pages/About.vue");
+const MovieDetail = () => import("../pages/MovieDetail.vue");
 
 const routes = [
   {
     name: "home",
     path: "/",
     component: Home,
+  },
+  {
+    name: "movieDetail",
+    path: "/movie/:id",
+    component: MovieDetail,
   },
   {
     name: "about",
