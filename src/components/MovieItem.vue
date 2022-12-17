27defb8b51a4d334a37db8878393125ec4afe864
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-17 14:16:14 +0500

    change movieItem

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,27 +1,30 @@
 <template>
-    <div
-        class="movie-item relative cursor-pointer rounded-md overflow-hidden transition-all duration-2000 ease-in h-64">
-        <div class="absolute inset-0 bg-no-repeat bg-cover bg-center -z-10" :style="posterBg">
-        </div>
+    <RouterLink :to="{ name: 'movieDetail', params: { id: movie.imdbID } }">
         <div
-            class="movie-info-wrap flex flex-col justify-between p-2 h-full opacity-0 transition-all duration-200 ease-in">
-            <div class="movie-item-info rounded-lg bg-gray-700">
-                <h3 class="movie-title text-center text-white">
-                    {{ movie.Title }},
-                </h3>
-                <p class="movie-year text-center text-white">
-                    {{ movie.Year }},
-                </p>
-                <p class="movie-year text-center text-white">
-                    {{ movie.Runtime }}
-                </p>
+            class="movie-item relative cursor-pointer rounded-md overflow-hidden transition-all duration-2000 ease-in h-64">
+            <div class="absolute inset-0 bg-no-repeat bg-cover bg-center -z-10" :style="posterBg">
+            </div>
+            <div
+                class="movie-info-wrap flex flex-col justify-between p-2 h-full opacity-0 transition-all duration-200 ease-in">
+                <div class="movie-item-info rounded-lg bg-gray-700">
+                    <h3 class="movie-title text-center text-white">
+                        {{ movie.Title }},
+                    </h3>
+                    <p class="movie-year text-center text-white">
+                        {{ movie.Year }},
+                    </p>
+                    <p class="movie-year text-center text-white">
+                        {{ movie.Runtime }}
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
+    </RouterLink>
 </template>
 
 <script setup>
 import { computed } from 'vue';
+import { RouterLink } from 'vue-router';
 
 const props = defineProps({
     movie: {
