829b922b3a4bbeaa066d1beaae6b18e7901b1abd
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-17 14:15:48 +0500

    create movieDetail

---
+++ src/pages/MovieDetail.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="goBack">
+        Go Back
+    </button>
+    <h2>
+        {{ movieDetail.Plot }}
+    </h2>
+</template>
+
+<script setup>
+import { onMounted, computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+import { useMovieStore } from '../stores/movies.js'
+
+const movieStore = useMovieStore()
+const route = useRoute()
+const router = useRouter()
+
+const movieDetail = computed(() => movieStore.movieDetail)
+
+function goBack() {
+    router.go(-1)
+}
+
+onMounted(() => {
+    movieStore.movieDetails(route)
+})
+</script>
