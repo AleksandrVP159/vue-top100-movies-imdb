78680de14ae185ca2f2fc4dc5279822dfbeb3d30
Aleksandr Petukhov <alexandr.9494@yandex.ru>
2022-12-17 14:16:02 +0500

    change Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,6 @@
     <MoviesList v-if="!isExistAll" @change-poster="onChangePoster" @change-out-poster="onChangeOutPoster" />
     <MovieListAll v-else @change-poster="onChangePoster" @change-out-poster="onChangeOutPoster" />
     <Auth v-if="modalStore.isOpen === true" />
-    <Search />
 </template>
 
 <script setup>
@@ -11,7 +10,7 @@ import { useModalStore } from '../stores/modal';
 import { useMovieStore } from '../stores/movies';
 import MoviesList from '../components/MoviesList.vue';
 import MovieListAll from '../components/MovieListAll.vue'
-import Search from '../components/Search.vue';
+/* import Search from '../components/Search.vue'; */
 import PosterBg from '../components/PosterBg.vue';
 import Auth from '../components/Auth.vue';
 
